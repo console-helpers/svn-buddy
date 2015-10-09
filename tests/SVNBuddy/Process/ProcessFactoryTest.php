5ab4e467e45e2958e83db69d72efb83ece429f08
Alexander Obuhovich <aik.bold@gmail.com>
2015-10-09 22:31:37 +0300

    Adding test for "ProcessFactory" class

---
+++ tests/SVNBuddy/Process/ProcessFactoryTest.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * This file is part of the SVN-Buddy library.
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @copyright Alexander Obuhovich <aik.bold@gmail.com>
+ * @link      https://github.com/aik099/svn-buddy
+ */
+
+namespace Tests\aik099\SVNBuddy\Process;
+
+
+use aik099\SVNBuddy\Process\ProcessFactory;
+
+class ProcessFactoryTest extends \PHPUnit_Framework_TestCase
+{
+
+	public function testImplementsCorrectInterface()
+	{
+		$this->assertInstanceOf('aik099\\SVNBuddy\\Process\\IProcessFactory', new ProcessFactory());
+	}
+
+	public function testProcessCanBeCreated()
+	{
+		$factory = new ProcessFactory();
+
+		$process = $factory->createProcess('command', 10);
+
+		$this->assertInstanceOf('Symfony\\Component\\Process\\Process', $process);
+		$this->assertEquals('command', $process->getCommandLine());
+		$this->assertEquals(10, $process->getIdleTimeout());
+		$this->assertNull($process->getTimeout());
+	}
+
+}
