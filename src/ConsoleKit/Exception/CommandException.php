6d5978a65f4900b497b1b084f7f456c71bdff2b2
Alexander Obuhovich <aik.bold@gmail.com>
2015-10-29 21:24:41 +0200

    Change url in license file comment of "ConsoleHelpers\ConsoleKit" namespace files

--- src/ConsoleKit/Exception/CommandException.php
+++ src/ConsoleKit/Exception/CommandException.php
@@ -5,7 +5,7 @@
  * the LICENSE file that was distributed with this source code.
  *
  * @copyright Alexander Obuhovich <aik.bold@gmail.com>
- * @link      https://github.com/console-helpers/svn-buddy
+ * @link      https://github.com/console-helpers/console-kit
  */
 
 namespace ConsoleHelpers\ConsoleKit\Exception;
