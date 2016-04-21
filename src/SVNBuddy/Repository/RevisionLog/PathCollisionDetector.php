269b7b3f10225b2e535cec427a536eb7d7d330c8
Alexander Obuhovich <aik.bold@gmail.com>
2016-04-21 20:50:09 +0300

    Added "PathCollisionDetector" class

---
+++ src/SVNBuddy/Repository/RevisionLog/PathCollisionDetector.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * This file is part of the SVN-Buddy library.
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @copyright Alexander Obuhovich <aik.bold@gmail.com>
+ * @link      https://github.com/console-helpers/svn-buddy
+ */
+
+namespace ConsoleHelpers\SVNBuddy\Repository\RevisionLog;
+
+
+class PathCollisionDetector
+{
+
+	/**
+	 * Known paths.
+	 *
+	 * @var array
+	 */
+	private $_knownPaths = array();
+
+	/**
+	 * Expanded array of paths.
+	 *
+	 * @var array
+	 */
+	private $_expandedPaths = array('/' => 1);
+
+	/**
+	 * Longest path length.
+	 *
+	 * @var integer
+	 */
+	private $_longestPathLength = 1;
+
+	/**
+	 * Adds paths.
+	 *
+	 * @param array $paths Paths.
+	 *
+	 * @return void
+	 */
+	public function addPaths(array $paths)
+	{
+		foreach ( $paths as $path ) {
+			$parent_path = rtrim($path, '/');
+			$this->_knownPaths[$path] = true;
+
+			do {
+				$this->_expandedPaths[$parent_path . '/'] = strlen($parent_path . '/');
+				$parent_path = dirname($parent_path);
+			} while ( $parent_path !== '/' && $parent_path !== '' );
+		}
+
+		$path_count = count($this->_expandedPaths);
+
+		if ( $path_count > 1 ) {
+			$this->_longestPathLength = call_user_func_array('max', $this->_expandedPaths);
+		}
+		elseif ( $path_count === 1 ) {
+			$this->_longestPathLength = current($this->_expandedPaths);
+		}
+		else {
+			$this->_longestPathLength = 0;
+		}
+	}
+
+	/**
+	 * Checks path collision.
+	 *
+	 * @param string $path Path.
+	 *
+	 * @return boolean
+	 */
+	public function isCollision($path)
+	{
+		if ( isset($this->_knownPaths[$path]) || !$this->_knownPaths ) {
+			return false;
+		}
+
+		$path = substr($path, 0, $this->_longestPathLength);
+
+		return isset($this->_expandedPaths[$path]);
+	}
+
+}
---
+++ tests/SVNBuddy/Repository/RevisionLog/PathCollisionDetectorTest.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * This file is part of the SVN-Buddy library.
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @copyright Alexander Obuhovich <aik.bold@gmail.com>
+ * @link      https://github.com/console-helpers/svn-buddy
+ */
+
+namespace Tests\ConsoleHelpers\SVNBuddy\Repository\RevisionLog;
+
+
+use ConsoleHelpers\SVNBuddy\Repository\RevisionLog\PathCollisionDetector;
+
+class PathCollisionDetectorTest extends \PHPUnit_Framework_TestCase
+{
+
+	/**
+	 * Path collision detector.
+	 *
+	 * @var PathCollisionDetector
+	 */
+	protected $pathCollisionDetector;
+
+	protected function setUp()
+	{
+		parent::setUp();
+
+		$this->pathCollisionDetector = new PathCollisionDetector();
+	}
+
+	public function testNoCollisionWithoutPaths()
+	{
+		$this->assertFalse($this->pathCollisionDetector->isCollision('/path/to/folder/'));
+	}
+
+	public function testNoCollisionForKnownPath()
+	{
+		$this->pathCollisionDetector->addPaths(array('/path/to/folder/'));
+
+		$this->assertFalse($this->pathCollisionDetector->isCollision('/path/to/folder/'));
+	}
+
+	public function testNoCollisionWithDifferentParentPath()
+	{
+		$this->pathCollisionDetector->addPaths(array('/path/to/folder/'));
+
+		$this->assertFalse($this->pathCollisionDetector->isCollision('/another-path/to/folder/'));
+	}
+
+	public function testNoCollisionForRootPathWithoutKnownPaths()
+	{
+		$this->assertFalse($this->pathCollisionDetector->isCollision('/'));
+	}
+
+	public function testCollisionForRootPathWithKnownPaths()
+	{
+		$this->pathCollisionDetector->addPaths(array('/path/to/project/'));
+
+		$this->assertTrue($this->pathCollisionDetector->isCollision('/'));
+	}
+
+	public function testCollisionOnParentPath()
+	{
+		$this->pathCollisionDetector->addPaths(array('/path/to/folder/'));
+
+		$this->assertTrue($this->pathCollisionDetector->isCollision('/path/to/'));
+	}
+
+	public function testCollisionOnChildPath()
+	{
+		$this->pathCollisionDetector->addPaths(array('/path/to/folder/'));
+
+		$this->assertTrue($this->pathCollisionDetector->isCollision('/path/to/folder/sub-folder/'));
+	}
+
+	public function testCollisionWithRootPathIsKnownPath()
+	{
+		$this->pathCollisionDetector->addPaths(array('/'));
+
+		$this->assertTrue($this->pathCollisionDetector->isCollision('/path/to/folder/'));
+	}
+
+	public function testNoCollisionWithRootPathIsKnownPath()
+	{
+		$this->pathCollisionDetector->addPaths(array('/'));
+
+		$this->assertFalse($this->pathCollisionDetector->isCollision('/'));
+	}
+
+}
