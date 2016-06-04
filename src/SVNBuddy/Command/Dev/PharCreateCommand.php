1413b80afa6e2edebe35383a430f6d3097be8774
Alexander Obuhovich <aik.bold@gmail.com>
2016-06-04 10:55:57 +0300

    The stability is now baked in into version of created phars

--- src/SVNBuddy/Command/Dev/PharCreateCommand.php
+++ src/SVNBuddy/Command/Dev/PharCreateCommand.php
@@ -11,7 +11,10 @@
 namespace ConsoleHelpers\SVNBuddy\Command\Dev;
 
 
+use ConsoleHelpers\ConsoleKit\Exception\CommandException;
 use ConsoleHelpers\SVNBuddy\Command\AbstractCommand;
+use ConsoleHelpers\SVNBuddy\Updater\Stability;
+use Stecman\Component\Symfony\Console\BashCompletion\CompletionContext;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -43,6 +46,13 @@ class PharCreateCommand extends AbstractCommand
 				InputOption::VALUE_REQUIRED,
 				'Directory, where build results would be stored',
 				'build'
+			)
+			->addOption(
+				'stability',
+				's',
+				InputOption::VALUE_REQUIRED,
+				'Stability of the build (<comment>stable</comment>, <comment>snapshot</comment>, <comment>preview</comment>)',
+				Stability::STABLE
 			);
 
 		parent::configure();
@@ -63,12 +73,41 @@ class PharCreateCommand extends AbstractCommand
 	}
 
 	/**
+	 * Return possible values for the named option
+	 *
+	 * @param string            $optionName Option name.
+	 * @param CompletionContext $context    Completion context.
+	 *
+	 * @return array
+	 */
+	public function completeOptionValues($optionName, CompletionContext $context)
+	{
+		$ret = parent::completeOptionValues($optionName, $context);
+
+		if ( $optionName === 'stability' ) {
+			return $this->_getStabilities();
+		}
+
+		return $ret;
+	}
+
+	/**
 	 * {@inheritdoc}
+	 *
+	 * @throws \RuntimeException When unknown stability was specified.
 	 */
 	protected function execute(InputInterface $input, OutputInterface $output)
 	{
+		$stability = $this->_getStability();
+
+		if ( !in_array($stability, $this->_getStabilities()) ) {
+			throw new \RuntimeException('The "' . $stability . '" is unknown.');
+		}
+
+		$version = $this->_getVersion();
 		$build_dir = realpath($this->io->getOption('build-dir'));
 
+		$this->io->write('1. removing dev dependencies ... ');
 		$this->_shellCommand(
 			'composer',
 			array(
@@ -78,12 +117,15 @@ class PharCreateCommand extends AbstractCommand
 			),
 			$this->_projectRootFolder
 		);
+		$this->io->writeln('done');
 
+		$this->io->write('2. creating phar file ... ');
 		$box_config = json_decode(file_get_contents($this->_projectRootFolder . '/box.json.dist'), true);
 
 		$phar_file = $build_dir . '/' . basename($box_config['output']);
 		$signature_file = $phar_file . '.sig';
 
+		$box_config['replacements'] = array('git-version' => $version);
 		$box_config['output'] = $phar_file;
 
 		file_put_contents(
@@ -93,12 +135,16 @@ class PharCreateCommand extends AbstractCommand
 
 		$box_cli = trim($this->_shellCommand('which', array('box')));
 		$this->_shellCommand('php', array('-d', 'phar.readonly=0', $box_cli, 'build'), $this->_projectRootFolder);
+		$this->io->writeln('done');
 
+		$this->io->write('3. calculating phar signature ... ');
 		file_put_contents(
 			$signature_file,
 			$this->_shellCommand('sha1sum', array(basename($phar_file)), dirname($phar_file))
 		);
+		$this->io->writeln('done');
 
+		$this->io->write('4. restoring dev dependencies ... ');
 		$this->_shellCommand(
 			'composer',
 			array(
@@ -107,8 +153,57 @@ class PharCreateCommand extends AbstractCommand
 			),
 			$this->_projectRootFolder
 		);
+		$this->io->writeln('done');
 
-		$this->io->writeln('Phar created successfully.');
+		$this->io->writeln('Phar for <info>' . $version . '</info> version created.');
+	}
+
+	/**
+	 * Returns stability.
+	 *
+	 * @return string|null
+	 */
+	private function _getStability()
+	{
+		return $this->io->getOption('stability');
+	}
+
+	/**
+	 * Returns all stabilities.
+	 *
+	 * @return array
+	 */
+	private function _getStabilities()
+	{
+		return array(Stability::PREVIEW, Stability::SNAPSHOT, Stability::STABLE);
+	}
+
+	/**
+	 * Returns version.
+	 *
+	 * @return string
+	 * @throws CommandException When "stable" stability was used with non-stable version.
+	 */
+	private function _getVersion()
+	{
+		$stability = $this->_getStability();
+		$git_version = $this->_getGitVersion();
+
+		if ( preg_match('/^.*-[\d]+-g.{7}$/', $git_version) && $stability === Stability::STABLE ) {
+			throw new CommandException('The "' . $git_version . '" version can\'t be used with "stable" stability.');
+		}
+
+		return $stability . ':' . $git_version;
+	}
+
+	/**
+	 * Returns same version as Box does for "git-version" replacement.
+	 *
+	 * @return string
+	 */
+	private function _getGitVersion()
+	{
+		return trim($this->_shellCommand('git', array('describe', 'HEAD', '--tags')));
 	}
 
 	/**
