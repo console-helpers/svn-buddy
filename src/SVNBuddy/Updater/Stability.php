c65f7a5af66ea16b3c4f80592b32fe607f424d41
Alexander Obuhovich <aik.bold@gmail.com>
2016-06-04 10:44:22 +0300

    Added central class for accessing stabilities

---
+++ src/SVNBuddy/Updater/Stability.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is part of the SVN-Buddy library.
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @copyright Alexander Obuhovich <aik.bold@gmail.com>
+ * @link      https://github.com/console-helpers/svn-buddy
+ */
+
+namespace ConsoleHelpers\SVNBuddy\Updater;
+
+
+class Stability
+{
+
+	const PREVIEW = 'preview';
+
+	const SNAPSHOT = 'snapshot';
+
+	const STABLE = 'stable';
+
+}
