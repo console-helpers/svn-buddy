30804202c4e5592ea435eb5f6f5828e20c177408
Alexander Obuhovich <aik.bold@gmail.com>
2016-06-04 18:03:49 +0300

    Use intellectual version matching when checking for updates

--- src/SVNBuddy/Updater/Updater.php
+++ src/SVNBuddy/Updater/Updater.php
@@ -12,6 +12,7 @@ namespace ConsoleHelpers\SVNBuddy\Updater;
 
 
 use Humbug\SelfUpdate\Updater as BaseUpdater;
+use Humbug\SelfUpdate\VersionParser;
 
 class Updater extends BaseUpdater
 {
@@ -26,11 +27,60 @@ class Updater extends BaseUpdater
 		$this->newVersion = $this->strategy->getCurrentRemoteVersion($this);
 		$this->oldVersion = $this->strategy->getCurrentLocalVersion($this);
 
-		if ( !empty($this->newVersion) && ($this->newVersion !== $this->oldVersion) ) {
+		list ($new_stability, $new_version) = $this->parseStability($this->newVersion);
+		list ($old_stability, $old_version) = $this->parseStability($this->oldVersion);
+
+		// Release with different stability a newer one.
+		if ( $new_stability !== $old_stability ) {
 			return true;
 		}
 
-		return false;
+		list ($new_version, $new_offset) = $this->parseOffset($new_version);
+		list ($old_version, $old_offset) = $this->parseOffset($old_version);
+
+		// Release made with larger offset is a newer one.
+		if ( $new_version === $old_version ) {
+			return $new_offset > $old_offset;
+		}
+
+		// Just see which version is larger.
+		$version_parser = new VersionParser(array($new_version, $old_version));
+
+		return $version_parser->getMostRecentAll() === $new_version;
+	}
+
+	/**
+	 * Returns version parsed into stability and actual version.
+	 *
+	 * @param string $version Version.
+	 *
+	 * @return array
+	 */
+	protected function parseStability($version)
+	{
+		$parts = explode(':', $version);
+
+		if ( count($parts) === 1 ) {
+			return array(Stability::STABLE, $parts[0]);
+		}
+
+		return $parts;
+	}
+
+	/**
+	 * Returns version parsed into tag and commit count after that tag.
+	 *
+	 * @param string $version Version.
+	 *
+	 * @return array
+	 */
+	protected function parseOffset($version)
+	{
+		if ( preg_match('/^(.*)-([\d]+)-g.{7}$/', $version, $regs) ) {
+			return array($regs[1], $regs[2]);
+		};
+
+		return array($version, 0);
 	}
 
 }
