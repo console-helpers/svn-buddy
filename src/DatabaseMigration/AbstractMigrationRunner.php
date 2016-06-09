331c95710c12584287dac53f3d8f22fae1ced37a
Alexander Obuhovich <aik.bold@gmail.com>
2016-06-09 21:39:12 +0300

    Preparing for moving DB migration classes into separate package: url/name change

--- src/DatabaseMigration/AbstractMigrationRunner.php
+++ src/DatabaseMigration/AbstractMigrationRunner.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * This file is part of the SVN-Buddy library.
+ * This file is part of the DB-Migration library.
  * For the full copyright and license information, please view
  * the LICENSE file that was distributed with this source code.
  *
  * @copyright Alexander Obuhovich <aik.bold@gmail.com>
- * @link      https://github.com/console-helpers/svn-buddy
+ * @link      https://github.com/console-helpers/db-migration
  */
 
 namespace ConsoleHelpers\DatabaseMigration;
