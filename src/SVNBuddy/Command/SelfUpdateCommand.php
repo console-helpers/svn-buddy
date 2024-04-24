8955267bfb03bf2a6085d6d41aaf79b541db695b
Alex <aik099@users.noreply.github.com>
2024-04-24 14:07:53 +0300

    Removed obsolete "ProcessBuilder" imports

--- src/SVNBuddy/Command/SelfUpdateCommand.php
+++ src/SVNBuddy/Command/SelfUpdateCommand.php
@@ -21,8 +21,6 @@ use ConsoleHelpers\SVNBuddy\Updater\VersionUpdateStrategy;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Process\PhpExecutableFinder;
-use Symfony\Component\Process\ProcessBuilder;
 
 class SelfUpdateCommand extends AbstractCommand
 {
