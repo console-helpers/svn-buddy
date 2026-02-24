35094c088cd32a629e839b6ca83172210b5ca740
Alex <aik099@users.noreply.github.com>
2026-02-24 11:12:12 +0200

    Corrected syntax of the Copyright comment

--- src/SVNBuddy/Config/ArrayConfigSetting.php
+++ src/SVNBuddy/Config/ArrayConfigSetting.php
@@ -1,5 +1,5 @@
 <?php
-/**
+/*
  * This file is part of the SVN-Buddy library.
  * For the full copyright and license information, please view
  * the LICENSE file that was distributed with this source code.
